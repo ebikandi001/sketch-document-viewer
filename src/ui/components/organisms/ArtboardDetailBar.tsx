1f4c134d831795c32e6238f9a141acdb4e5f890b
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-19 14:37:49 +0100

    [Organism]: ArtboardDetailBar

---
+++ src/ui/components/organisms/ArtboardDetailBar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CloseButton, Navigation } from '../molecules';
+import { Label } from '../atoms';
+
+type DetailBarProps = {
+  onClose: any;
+  goPrev: any;
+  goNext: any;
+  text: string;
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1%;
+  & > * {
+    margin: 0 1%;
+  }
+
+  .detail-bar__navigation {
+    width: 10%;
+  }
+
+  .detail-bar__label {
+    flex-grow: 1;
+    text-align: center;
+  }
+`;
+
+export const ArtboardDetailBar = ({
+  onClose,
+  goPrev,
+  goNext,
+  text,
+}: DetailBarProps) => (
+  <Container className="detail-bar">
+    <CloseButton className="detail-bar__close" onClick={onClose} />
+    <Navigation
+      className="detail-bar__navigation"
+      goPrev={goPrev}
+      goNext={goNext}
+    />
+    <Label className="detail-bar__label" text={text} isBold={true} />
+  </Container>
+);
---
+++ src/ui/components/organisms/index.ts
@@ -0,0 +1,1 @@
+export { ArtboardDetailBar } from './ArtboardDetailBar';
