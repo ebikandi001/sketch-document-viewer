0ddb62ca96c3ad864e8520a29cc9a76c0134c0b0
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-19 12:10:58 +0100

    [Molecules]: CloseButton

--- src/ui/components/molecules/CloseButton.tsx
+++ src/ui/components/molecules/CloseButton.tsx
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 import { Image } from '../atoms';
 import Close from '../assets/close.svg';
 
+type CloseButtonProps = {
+  onClose: any;
+};
+
 const CloseContainer = styled.div`
   border: solid;
 `;
 
-export const CloseButton = () => (
+export const CloseButton = ({ onClose }: CloseButtonProps) => (
   <CloseContainer className="closebutton">
-    <Image src={Close} className="closebutton__button" />
+    <Image src={Close} onClick={onClose} className="closebutton__button" />
   </CloseContainer>
 );
