3b6b6471ff17dcb16d102e305020e22cda1598b5
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-19 10:07:54 +0100

    [Image]: make it clickable

--- src/ui/components/atoms/Image.tsx
+++ src/ui/components/atoms/Image.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 export type ImageProps = {
   src: string;
   alt: string;
+  onClick?: () => {};
 };
 
-export const Image = (props: any) => <img src={props.src} alt={props.alt} />;
+export const Image = ({ src, alt, onClick }: any) => {
+  const handleClick = (...args: any[]) => {
+    try {
+      onClick(args);
+    } catch (e) {
+      console.error('[Image]: Bad onClick', e);
+    }
+  };
+
+  return <img src={src} alt={alt} onClick={handleClick} />;
+};
