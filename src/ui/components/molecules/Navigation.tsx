eef583237688fcd9b91602efc1fce9f2a096c218
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-19 12:18:53 +0100

    [Navigation]: Prev/Next handlers and CSS classnames

--- src/ui/components/molecules/Navigation.tsx
+++ src/ui/components/molecules/Navigation.tsx
@@ -5,20 +5,25 @@ import ArrowLeft from '../assets/arrow-left.svg';
 import ArrowRight from '../assets/arrow-right.svg';
 import Breadcrumb from '../assets/breadcrumb.svg';
 
+type NavigationProps = {
+  goPrev: any;
+  goNext: any;
+};
+
 const NavigationContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  max-width: 8vw;
+  width: 8vw;
 `;
 
-export const Navigation = () => (
-  <NavigationContainer>
-    <Image src={ArrowLeft} />
+export const Navigation = ({ goPrev, goNext }: NavigationProps) => (
+  <NavigationContainer className="navigation">
+    <Image src={ArrowLeft} className="navigation__left" onClick={goPrev} />
     4
-    <Image src={Breadcrumb} />
+    <Image src={Breadcrumb} className="navigation__separator" />
     10
-    <Image src={ArrowRight} />
+    <Image src={ArrowRight} className="navigation__right" onClick={goNext} />
   </NavigationContainer>
 );
