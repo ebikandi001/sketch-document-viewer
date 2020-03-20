536645757abd44637566f41144f9e2254cd217a1
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-20 17:16:18 +0100

    [Layout]: extracted useAspectRatio to a file + simplify ratio responsivenes

--- src/ui/components/templates/Layout.tsx
+++ src/ui/components/templates/Layout.tsx
@@ -1,79 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-
-// Credits: https://usehooks.com/useWindowSize/
-const useWindowSize = () => {
-  const isClient = typeof window === 'object';
-
-  function getSize() {
-    return {
-      windowWidth: isClient ? window.innerWidth - 10 : undefined,
-      windowHeight: isClient ? window.innerHeight - 10 : undefined,
-    };
-  }
-
-  const [windowSize, setWindowSize] = useState(getSize);
-
-  useEffect(() => {
-    if (!isClient) {
-      return undefined;
-    }
-
-    function handleResize() {
-      setWindowSize(getSize());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []); // Empty array ensures that effect is only run on mount and unmount
-
-  return windowSize;
-};
-// ------------------
+import { useAspectRatio } from './useAspectRatio';
 
 type LayoutProps = {
   width: string;
   height: string;
 };
 
-const FlexColumn = styled.div`
-  display: flex:
-  flex-direction: column;
-  align-items: center;
-  flex-grow: 1;
+const Grid = styled.div`
+  display: grid;
+  height: 100%;
+  justify-items: center;
+  justify-content: center;
 `;
 
 const LayoutContent = styled.div`
   background-color: white;
   border: solid green;
+  align-self: center;
   width: ${(props: LayoutProps) => props.width};
   height: ${(props: LayoutProps) => props.height};
 `;
 
-const getAspectRatio = ({ windowWidth, windowHeight }: any) => {
-  const RATIO = 1.6;
-  const aspectWidth = windowHeight * RATIO;
-
-  if (aspectWidth > windowWidth) {
-    return {
-      width: `${windowWidth}px`,
-      height: `${windowWidth / RATIO}px`,
-    };
-  } else {
-    return {
-      width: `${aspectWidth}px`,
-      height: `${windowHeight}px`,
-    };
-  }
-};
-
 export const Layout = () => {
-  const size = useWindowSize();
-  const { width, height } = getAspectRatio(size);
+  const { width, height } = useAspectRatio();
 
   return (
-    <FlexColumn>
+    <Grid>
       <LayoutContent className="layout" width={width} height={height} />
-    </FlexColumn>
+    </Grid>
   );
 };
---
+++ src/ui/components/templates/useAspectRatio.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+
+export const useAspectRatio = () => {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      windowWidth: isClient ? window.innerWidth - 10 : 0,
+      windowHeight: isClient ? window.innerHeight - 10 : 0,
+    };
+  }
+
+  const getAspectRatio = () => {
+    const { windowWidth, windowHeight } = getSize();
+    const RATIO = 1.6;
+    const aspectWidth = windowHeight * RATIO;
+
+    if (aspectWidth > windowWidth) {
+      return {
+        width: `${windowWidth}px`,
+        height: `${windowWidth / RATIO}px`,
+      };
+    } else {
+      return {
+        width: `${aspectWidth}px`,
+        height: `${windowHeight}px`,
+      };
+    }
+  };
+
+  const [aspectRatio, setAspectRatio] = useState(getAspectRatio);
+
+  useEffect(() => {
+    if (!isClient) {
+      return undefined;
+    }
+
+    function handleResize() {
+      setAspectRatio(getAspectRatio());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return aspectRatio;
+};
