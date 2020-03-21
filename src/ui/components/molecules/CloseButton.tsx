fd53d61f15972fd24cf7cf9a0d15bf38f4330547
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-21 14:23:42 +0100

    [CloseButton]: remove unnecesary Div

--- src/ui/components/molecules/CloseButton.tsx
+++ src/ui/components/molecules/CloseButton.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Image } from '../atoms';
 import { ComponentStyleProps } from '../theme';
 import Close from '../assets/close.svg';
@@ -8,10 +7,6 @@ type CloseButtonProps = ComponentStyleProps & {
   onClick: any;
 };
 
-const CloseContainer = styled.div``;
-
 export const CloseButton = ({ onClick, className }: CloseButtonProps) => (
-  <CloseContainer className={className}>
-    <Image src={Close} onClick={onClick} className={`${className}__button`} />
-  </CloseContainer>
+  <Image src={Close} onClick={onClick} className={`${className}__button`} />
 );
