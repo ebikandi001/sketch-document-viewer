d1bdda8712dd3fc48bbacac1f8e24f19a06a6952
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-21 11:12:26 +0100

    [Layout]: simplify component

--- src/ui/components/templates/Layout.tsx
+++ src/ui/components/templates/Layout.tsx
@@ -18,24 +18,23 @@ const Grid = styled.div`
   height: 100%;
   justify-items: center;
   justify-content: center;
-`;
-
-const LayoutAspectRatio = styled.div`
-  background-color: white;
-  align-self: center;
-  width: ${(props: AspectRatioProps) => props.width};
-  height: ${(props: AspectRatioProps) => props.height};
-  display: grid;
-  grid-template-rows: 7% 93%;
-  align-items: stretch;
-  .layout__header {
-    background-color: rgba(247, 247, 247, 1);
-  }
-  .layout__content {
-    background-image: linear-gradient(rgba(235, 235, 235, 1), white);
-    box-shadow: 0px 1px 8px #999999 inset;
-    overflow-x: hidden;
-    overflow-y: scroll; /* TODO: Is this needed? no scrollbar on example Document*/
+  .layout {
+    background-color: white;
+    align-self: center;
+    width: ${(props: AspectRatioProps) => props.width};
+    height: ${(props: AspectRatioProps) => props.height};
+    display: grid;
+    grid-template-rows: 7% 93%;
+    align-items: stretch;
+    .layout__header {
+      background-color: rgba(247, 247, 247, 1);
+    }
+    .layout__content {
+      background-image: linear-gradient(rgba(235, 235, 235, 1), white);
+      box-shadow: 0px 1px 8px #999999 inset;
+      overflow-x: hidden;
+      overflow-y: scroll; /* TODO: Is this needed? no scrollbar on example Document*/
+    }
   }
 `;
 
@@ -43,11 +42,11 @@ export const Layout = ({ header, content }: LayoutProps) => {
   const { width, height } = useAspectRatio();
 
   return (
-    <Grid>
-      <LayoutAspectRatio className="layout" width={width} height={height}>
+    <Grid width={width} height={height}>
+      <div className="layout">
         <div className="layout__header">{header || 'Place header here'}</div>
         <div className="layout__content">{content || 'Place content here'}</div>
-      </LayoutAspectRatio>
+      </div>
     </Grid>
   );
 };
