5284b0cf0c5e2061375d81841b46e293a7d579a8
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-22 10:34:41 +0100

    [Image]: remove unused onClick porp

--- src/ui/components/atoms/Image.tsx
+++ src/ui/components/atoms/Image.tsx
@@ -5,31 +5,17 @@ import { ComponentStyleProps } from '../theme';
 export type ImageProps = ComponentStyleProps & {
   src: string;
   alt?: string;
-  onClick?: any;
 };
 
 const ImageContainer = styled.div`
   img {
     max-width: 100%;
     height: auto;
-    &.clickable {
-      cursor: ${({ onClick }: any) => (onClick ? 'pointer' : 'auto')};
-    }
   }
 `;
 
-export const Image = ({ className, src, alt = '', onClick }: ImageProps) => {
-  const handleClick = (...args: any[]) => {
-    try {
-      onClick(args);
-    } catch (e) {
-      console.warn('[Image]: Bad onClick', e);
-    }
-  };
-
-  return (
-    <ImageContainer onClick={handleClick} className={className}>
-      <img src={src} alt={alt} className={onClick ? 'clickable' : ''} />
-    </ImageContainer>
-  );
-};
+export const Image = ({ className, src, alt = '' }: ImageProps) => (
+  <ImageContainer className={className}>
+    <img src={src} alt={alt} />
+  </ImageContainer>
+);
