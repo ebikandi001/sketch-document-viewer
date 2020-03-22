c81e0c2634ab301516f36c630d3206ccbc6f6608
Eneko Bikandi <eneko.bikandi@ve.com>
2020-03-22 10:29:46 +0100

    [Document]: go for another one!

--- src/ui/components/organisms/DocumentTitle.tsx
+++ src/ui/components/organisms/DocumentTitle.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Image, Label } from '../atoms'; // TODO: organism importing atoms?
 import styled from 'styled-components';
 import SketchLogo from '../assets/sketch-logo.svg';
+import { Link } from 'react-router-dom';
 
 type TitleProps = {
   text: string;
+  link?: string;
 };
 
 const TitleContainer = styled.div`
@@ -20,14 +22,25 @@ const TitleContainer = styled.div`
     }
   }
   .title__label {
-    margin-left: 3%;
+    flex-grow: 1;
+    padding-left: 3%;
     text-align: left;
+  }
+  .link {
+    flex-shrink: 1;
+    text-decoration: none;
+    color: grey;
   }
 `;
 
-export const DocumentTitle = ({ text = '' }: TitleProps) => (
+export const DocumentTitle = ({ text, link }: TitleProps) => (
   <TitleContainer className="title">
     <Image src={SketchLogo} className="title__logo" />
     <Label text={text} isBold={true} className="title__label" />
+    {link ? (
+      <Link to={link} className="link">
+        Go for another one!
+      </Link>
+    ) : null}
   </TitleContainer>
 );
--- src/ui/pages/DocumentView.tsx
+++ src/ui/pages/DocumentView.tsx
@@ -3,17 +3,26 @@ import { getDocument, useDocumentState, useDocumentDispatch } from 'adapters';
 import { DocumentTitle, ThumbnailGrid } from '../components/organisms';
 import { Layout } from '../components/templates';
 
-export const DocumentView = (props: any) => {
+const getNextDocLink = (currentId: string | undefined) => {
+  const nextId = ['Y8wDM', '4W43q'].find(id => currentId !== id);
+  return `/document/${nextId}`;
+};
+
+export const DocumentView = ({
+  match: {
+    params: { id },
+  },
+}: any) => {
   const doc = useDocumentState();
   const dispatch = useDocumentDispatch();
-
   useEffect(() => {
-    if (!doc?.shortId) {
-      getDocument('Y8wDM', dispatch);
+    if (!doc?.shortId || !doc?.shortId !== id) {
+      getDocument(id, dispatch);
     }
-  }, []);
+  }, [id]);
 
-  const header = <DocumentTitle text={doc?.name || ''} />;
+  const nextDocLink = getNextDocLink(doc?.shortId);
+  const header = <DocumentTitle text={doc?.name || ''} link={nextDocLink} />;
   const content = <ThumbnailGrid items={doc?.artboards || []} />;
 
   return <Layout header={header} content={content} />;
